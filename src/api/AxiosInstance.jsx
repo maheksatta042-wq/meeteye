80e75e6d94692108e0c9c92124ebdb1006d3383a
MrunmayeeCom <mrunmayeecom@gmail.com>
2026-02-06 18:20:37 +0530

    changes

--- src/api/AxiosInstance.jsx
+++ src/api/AxiosInstance.jsx
@@ -18,23 +18,3 @@ API.interceptors.request.use((config) => {
 });
 
 export default API;
-
-
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default API;
